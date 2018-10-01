b092983d6cad432977681a9f8484b4bb20722ed6
riya <rg329@njit.edu>
2018-09-30 21:10:46 -0400

    Fix: Edit Homepage text -- created contructor and first read class to read the file

--- views/index.php
+++ views/index.php
@@ -6,46 +6,28 @@
  * Time: 4:39 PM
  */
 -->
-<!DOCTYPE html>
-<html>
-<head>
-    {% include 'header.html' %}
-
-</head>
-<body>
-
-<div>
-
-
-<table class="yellowTable">
-
 <?php
-/** wrong file errors  */
 
-if(isset($_GET['file_error']))
-    if($_GET('file_error'))
-        echo "<tr>
-        <td>Error: Format of the file is wrong. Only .CSV file can be uploaded</td><br>
-        </tr></td>";
+class main {
+    /*create contructor*/
+    public function_construct() {
+        $csv_file_data = file_read::csv_file_read();
+        
+
+    }
+}
+
+/*  create class to read the CSV file */
+class file_read {
+    static public funtion csv_file_read() {
+        $name = 'project1.csv';
+        $csv = fileopen($name, 'f');
+        return $csv;
+    }
+}
+
+
+
+
 ?>
-<tr>
-<td>
-<h2><a href="index.php"> Please Resubmit This Page </a><br><br>
-<form action="projectupload.php" method="post" enctype="multipart/form-data" name="project1" id="project1">
-    <label for="selectfile">Browse to select file</label>
-    <input type="file" name="selectfile" id="selectfile" />
-    <input type="submit" name="Import" value="Import" />
-        <h3>Format of the file must be in <b>.CSV</b></h3></h2>
-
-</form>
-</td>
-</tr>
-</table>
-</div>
-
-<!--create footer -->
-<div id="Footer"></div>
-
-
-</body>
-</html>
+}
