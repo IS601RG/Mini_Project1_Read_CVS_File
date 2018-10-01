d872f07fa6a04a049c23680a5185e2f12125fda4
riya <rg329@njit.edu>
2018-09-30 22:03:12 -0400

    Fix: Edit Homepage text

--- views/index.php
+++ views/index.php
@@ -6,12 +6,22 @@
  * Time: 4:39 PM
  */
 -->
+<style>
+    .tab, .h{
+        border: 2px solid darkslategray;
+    }
+
+</style>
+
 <?php
+    $first = new main();
 
 class main {
     /*create contructor*/
     public function_construct() {
         $csv_file_data = file_read::csv_file_read();
+        $print_table = html::show_table($csv_file_data);
+        system::display_csv_file_data($print_table);
         
 
     }
@@ -21,13 +31,37 @@ class main {
 class file_read {
     static public funtion csv_file_read() {
         $name = 'project1.csv';
-        $csv = fileopen($name, 'f');
+        $csv = fopen($name, 'k');
         return $csv;
     }
 }
+/* create class to display table */
+class html {
+    static public function print_table($csv, $header = false) {
+        echo '<html>
+               <body><table class="tab">';
 
+        while ($file_content = fgetcsv($csv, 5000, ',')) {
+            echo '<tr>';
+            if ($header){
+                foreach ($file_content as $file_header){
+                    echo "<th class='h'>$file_header</th>";
+                }
+            }
+          else {
+              /*content for column*/
+              foreach ($file_content as $file_column) {
+                  echo "<td class='h'>$file_column</td>";
+              }
+          }
+        }
+        echo '<tr>';
+    }
+        }
+        echo '</html></table></body>';
+        fclose($csv);
 
-
+    }
+}
 
 ?>
-}
