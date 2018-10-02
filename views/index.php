e57427cc25e9319c47f457fbd3f53a1ba94d2350
riya <rg329@njit.edu>
2018-10-01 22:45:19 -0400

    Fix: Edit Homepage text/updated projectupload file

--- views/index.php
+++ views/index.php
@@ -6,62 +6,52 @@
  * Time: 4:39 PM
  */
 -->
-<style>
-    .tab, .h{
-        border: 2px solid darkslategray;
-    }
 
-</style>
+<!--
+<!DOCTYPE html>
+<html>
+<head>
+    {% include 'header.html' %}
 
-<?php
-    $first = new main();
+</head>
+-->
+<body>
 
-class main {
-    /*create contructor*/
-    public function_construct() {
-        $csv_file_data = file_read::csv_file_read();
-        $print_table = html::show_table($csv_file_data);
-        system::display_csv_file_data($print_table);
-        
+<div>
+    <table class="yellowTable">
 
-    }
-}
+        <?php
+        /** wrong file errors  */
 
-/*  create class to read the CSV file */
-class file_read {
-    static public funtion csv_file_read() {
-        $name = 'project1.csv';
-        $csv = fopen($name, 'k');
-        return $csv;
-    }
-}
-/* create class to display table */
-class html {
-    static public function print_table($csv, $header = false) {
-        echo '<html>
-               <body><table class="tab">';
+        if(isset($_GET['file_error']))
+            if($_GET('file_error'))
+                echo "<tr>
+        <td>Error: Format of the file is wrong. Only .CSV file can be uploaded</td><br>
+        </tr></td>";
+        ?>
+        <tr>
+            <td>
+                <h2><a href="index.php"> Please Resubmit This Page </a><br><br><br>
 
-        while ($file_content = fgetcsv($csv, 5000, ',')) {
-            echo '<tr>';
-            if ($header){
-                foreach ($file_content as $file_header){
-                    echo "<th class='h'>$file_header</th>";
-                }
-            }
-          else {
-              /*content for column*/
-              foreach ($file_content as $file_column) {
-                  echo "<td class='h'>$file_column</td>";
-              }
-          }
-        }
-        echo '<tr>';
-    }
-        }
-        echo '</html></table></body>';
-        fclose($csv);
+                    <form action="projectupload.php" method="post" enctype="multipart/form-data" name="project1" id="project1">
+                        <label for="Selectfile">Browse to select file</label>
+                        <input type="file" name="selectfile" id="Selectfile" /><br><br><br>
+                        <input type="submit" name="submit" value="Submit" />
+                        <h3>Format of the file must be in <b>.CSV</b></h3></h2>
+                 <!--
+                <?php
+                echo "SYSTEM TEMPORARY DIRECTORY IS: ". sys_get_temp_dir()."<br>";
+                ?>
+                -->
+                </form>
+            </td>
+        </tr>
+    </table>
+</div>
 
-    }
-}
+<!--create footer -->
+<div id="Footer"></div>
 
-?>
+
+</body>
+</html>
--- views/projectupload.php
+++ views/projectupload.php
@@ -1,11 +1,100 @@
-
-
-
-
-<?php
-/**
+<!--
  * Created by PhpStorm.
  * User: riya
  * Date: 9/29/18
  * Time: 5:28 PM
- */
+
+
+<style>
+    .table, .t {
+        border: 2px solid darkslategray;
+    }
+
+</style>
+-->
+
+<?php
+    $first = new main();
+
+class main {
+    /*create contructor*/
+    public function _construct() {
+        $readcvs = file_read::getreadcvs();
+        $displaytable = html::printTable($readcvs);
+        system::outputTemplate($displaytable);
+
+        /*
+        $csv_file_data = file_read::csv_file_read();
+        $print_table = html::print_table($csv_file_data);
+        system::display_csv_file_data($print_table); */
+
+
+    }
+}
+
+/*  create class to read the CSV file */
+class file_read {
+    static public function getreadcvs() {
+        $fname = 'project1.csv';
+        $csv = fopen($fname, "r");
+        return $csv;
+    }
+}
+/* create class to display table */
+class html {
+    static public function printTable($cvs) {
+
+        $displaytable = '<html><body><table class="table table-bordered">';
+
+        while ($input = fgetcsv($cvs, 5000, ',')) {
+
+            $displaytable .= '<tr>';
+
+            foreach ($input as $column) {
+
+                $displaytable .="<td>$column</td>";
+            }
+
+            $displaytable .= '</tr>';
+
+            }
+
+            $displaytable .= '</table></body></html>';
+            fclose($cvs);
+            return $displaytable;
+
+    }
+}
+class system {
+
+    static public function outputTemplate($template){
+
+        echo $template;
+    }
+
+
+        /* per TA Kishore -- this part needs clearificaiton
+        while ($file_content = fgetcsv($csv, 5000, ',')) {
+            echo '<tr>';
+            if ($header){
+                foreach ($file_content as $file_header){
+                    echo "<th class='t'>$file_header</th>";
+                }
+            }
+          else {
+              /*content for column
+              foreach ($file_content as $file_column) {
+                  echo "<td class='t'>$file_column</td>";
+              }
+          }
+        }
+        echo '<tr>';
+    }
+        }
+        echo '</html></table></body>';
+        fclose($csv);*/
+
+}
+
+
+?>
