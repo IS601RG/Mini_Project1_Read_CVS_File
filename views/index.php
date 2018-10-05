243818a2f69704d08cb0cbc027c2b532398b449d
riya <rg329@njit.edu>
2018-10-05 08:10:08 -0400

    Fix: created homepage to upload the file

--- views/index.php
+++ views/index.php
@@ -15,6 +15,7 @@
 
 </head>
 -->
+<html>
 <body>
 
 <div>
@@ -33,25 +34,21 @@
             <td>
                 <h2><a href="index.php"> Please Resubmit This Page </a><br><br><br>
 
-                    <form action="projectupload.php" method="post" enctype="multipart/form-data" name="project1" id="project1">
-                        <label for="Selectfile">Browse to select file</label>
-                        <input type="file" name="selectfile" id="Selectfile" /><br><br><br>
+                 <!--   <h2><br> Format of the file must be in <b>.CSV</b></h2></h2><br><br><br> -->
+
+                    <form action="projectupload.php" method="post" enctype="multipart/form-data" name="file1" id="file1">
+                        <label for="file">Browse to select file</label>
+                        <input type="file" name="file" id="file" /><br><br><br>
                         <input type="submit" name="submit" value="Submit" />
-                        <h3>Format of the file must be in <b>.CSV</b></h3></h2>
-                 <!--
-                <?php
-                echo "SYSTEM TEMPORARY DIRECTORY IS: ". sys_get_temp_dir()."<br>";
-                ?>
-                -->
-                </form>
+                    </form>
+
             </td>
         </tr>
     </table>
 </div>
 
-<!--create footer -->
-<div id="Footer"></div>
-
+<!--create footer
+<div id="Footer"></div> -->
 
 </body>
 </html>
