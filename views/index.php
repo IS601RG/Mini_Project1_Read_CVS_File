95caa3ad49f9c1284a22272a4cbe7b14cc1eca26
riya <rg329@njit.edu>
2018-10-06 17:29:35 -0400

    Task: fix the errors for returnArray()

--- views/index.php
+++ views/index.php
@@ -58,6 +58,7 @@ class html {
         $count = 0;
         foreach ($fileinput as $input) {
             $array = $input->returnArray();
+            $array = $input->returnArray();
             if($count == 0) {
                 $fieldkeys = array_keys($array);
                 $output = self:: getString($fieldkeys, $output);
@@ -78,20 +79,36 @@ class html {
         return $output;
     }
     public static function getHTMLHeader(){
-        $output =
+        $output = <<<'TAG'
+<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script><head><body><table class="table" table-bordered table-striped">';
+TAG;
 
         return $output;
     }
 }
 
-
-
-
-
 class recordFactory {
-    public static function create(Array $field = null, Array $input = null){
-        $input = new input($field, $input);
+    public static function create(Array $field = null, Array $values = null){
+        $input = new input($field, $values);
         return $input;
+    }
+}
+class input{
+    public function __construct(Array $field = null, $values = null)
+    {
+        $input = array_combine($field, $values);
+        foreach ($input as $property => $values) {
+            $this->createProperty($property, $values);
+        }
+    }
+    public function returnArray() {
+        $array = (array) $this;
+        return $array;
+    }
+    public function createProperty($name = 'Name', $value = 'Kevin L'){
+        $this->{$name} = $value;
     }
 }
 ?>
