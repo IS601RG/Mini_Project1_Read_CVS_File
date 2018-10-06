5c4a4f621a3e5c169957c53b6d91fa3f7972ecfb
riya <rg329@njit.edu>
2018-10-06 18:05:22 -0400

    Update: still having returnArray error.

--- views/index.php
+++ views/index.php
@@ -1,114 +1,91 @@
-<!--
- * Created by PhpStorm.
+<?php
+/*
+* Created by PhpStorm.
  * User: riya
  * Date: 9/29/18
  * Time: 5:28 PM
--->
-
-<html>
-
-<title>Mini Project 1 - IS601:101 by Riya Gandhi</title>
-
-
-
-</html>
-
-
-<?php
+ */
 
 main::start("project1.csv");
-
 class main{
-
-    static public function start($projectfile) {
-
-        $fileinput = csv::getRecords($projectfile);
-        $output = html::generateTable($fileinput);
-        echo $output;
+    static public function start($projectN){
+        $fileinput = csv::getRecords($projectN);
+        $table = html::generateTable($fileinput);
+        echo $table;
     }
 }
-
-/*create class to get record from file*/
-class csv{
-    static public function getRecords($projectfile) {
-        $readfile = fopen($projectfile, 'r');
-        $field = array();
-        $fileinput = array();
+class html {
+    public static function generateTable($fileinput) {
+        $table = self::getHeader();
         $count = 0;
-
-        while(!feof($readfile)){
-            $input = fgetcsv($readfile, 5000,',');
-            if($count == 0){
-              $field = $input;
+        foreach ($fileinput as $record) {
+            $array = $record->returnArray();
+            if($count == 0) {
+                $fields = array_keys($array);
+                $table = self::getString($fields, $table);
             }
-            else{
-                $fileinput[]=recordFactory::create($field,$input);
+            $values = array_values($array);
+            $table = self::getString($values, $table);
+            $count++;
+        }
+        $table.='</table></body></html>';
+        return $table;
+    }
+    public static function getString($array, $table){
+        $table.='<tr>';
+        foreach($array as $value){
+            $table .= $value;
+        }
+        $table.= '</tr>';
+        return $table;
+    }
+    public static function getHeader(){
+        $table = '<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+                    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script></head><body><table class="table table-bordered table-striped">';
+        return $table;
+    }
+}
+class csv{
+    static public function getRecords($projectN) {
+        $readfile = fopen($projectN,"r");
+        $field = array();
+        $count = 0;
+        while(! feof($readfile))
+        {
+            $record = fgetcsv($readfile);
+            if($count == 0) {
+                $field = $record;
+            } else {
+                $fileinput[] = recordFactory::create($field, $record);
             }
             $count++;
         }
         fclose($readfile);
         return $fileinput;
-        }
-}
-
-/*create class to display table*/
-class html {
-    public static function generateTable($fileinput){
-        $output = self::getHTMLHeader();
-        $count = 0;
-        foreach ($fileinput as $input) {
-            $array = $input->returnArray();
-            $array = $input->returnArray();
-            if($count == 0) {
-                $fieldkeys = array_keys($array);
-                $output = self:: getString($fieldkeys, $output);
-                }
-            $values = array_values($array);
-            $output = self::getString($values,$output);
-            $count++;
-         }
-         $output.='</table></body></html>';
-         return $output;
-    }
-    public static function getString($array, $output){
-        $output.='<tr>';
-        foreach($array as $value){
-            $output .= $value;
-        }
-        $output.='</tr>';
-        return $output;
-    }
-    public static function getHTMLHeader(){
-        $output = <<<'TAG'
-<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script><head><body><table class="table" table-bordered table-striped">';
-TAG;
-
-        return $output;
     }
 }
-
-class recordFactory {
-    public static function create(Array $field = null, Array $values = null){
-        $input = new input($field, $values);
-        return $input;
-    }
-}
-class input{
-    public function __construct(Array $field = null, $values = null)
+class record {
+    public function __construct(Array $field = null, $values = null )
     {
-        $input = array_combine($field, $values);
-        foreach ($input as $property => $values) {
-            $this->createProperty($property, $values);
+        $record = array_combine($field, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
         }
     }
     public function returnArray() {
         $array = (array) $this;
         return $array;
     }
-    public function createProperty($name = 'Name', $value = 'Kevin L'){
+
+    public function createProperty($name = 'Name', $value = 'Kevin L') {
+
         $this->{$name} = $value;
     }
 }
-?>
+class recordFactory {
+    public static function create(Array $field = null, Array $values = null) {
+        $record = new record($field, $values);
+        return $record;
+    }
+}
